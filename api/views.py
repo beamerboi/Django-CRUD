ce3902824d2acfc4afd683c35cff2581cdacbfbf
Ghassen Jemaî <hiallnigas@gmail.com>
2022-03-30 13:57:24 +0100

    fixes Updated and Destroy API views and add perform methods inside Destroy, Update and Create API View, add new function based view that manages the Create, List and Detail operations using POST and GET requests in the same time

--- api/views.py
+++ api/views.py
@@ -1,6 +1,9 @@
 from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
+from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
 
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
@@ -12,6 +15,13 @@ class ProductCreateAPIView(generics.CreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
+    def perform_create(self, serializer):
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content = title
+        serializer.save(content=content)
+
 
 class ProductListAPIView(generics.ListAPIView):
     queryset = Product.objects.all()
@@ -21,8 +31,42 @@ class ProductListAPIView(generics.ListAPIView):
 class ProductDeleteAPIView(generics.DestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def perform_destroy(self, instance):
+        super().perform_destroy(instance)
 
 
 class ProductUpdateAPIView(generics.UpdateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def perform_update(self, serializer):
+        serializer.save()
+        if not serializer.content:
+            serializer.content =serializer.title
+
+
+@api_view(['GET', 'POST'])
+def create_product_alt(request, pk=None, *args, **kwargs):
+    if request.method == "GET":
+        if pk is not None:
+            queryset = get_object_or_404(Product, pk)
+            serializer = ProductSerializer(queryset, many=False)
+            return Response(serializer.data, status=200)
+        queryset = Product.objects.all()
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data, status=200)
+
+    if request.method == "GET":
+        serializer = ProductSerializer(request.data)
+        if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data.get('title')
+            content = serializer.validated_data.get('content') or None
+            if content is None:
+                content = title
+            serializer.save(content=content)
+            return Response(serializer.data, status=201)
+
+
